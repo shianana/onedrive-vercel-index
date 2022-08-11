da007a591646133fb9a086cac1047c51e37da965
shianana <110763630+shianana@users.noreply.github.com>
2022-08-11 21:26:17 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -17,7 +17,7 @@ module.exports = {
   kvPrefix: process.env.KV_PREFIX || '',
 
   // The name of your website. Present alongside your icon.
-  title: "OneDrive",
+  title: "OneDrive-lyh",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
   baseDirectory: '/Public',
